ab3f24e8edf4cdc0861e2ae761b23e861d4c0220
Adityadevops7818 <kothajayaaditya7818@gmail.com>
2025-11-11 13:47:48 +0530

    Update py.java

--- py.java
+++ py.java
@@ -34,3 +34,30 @@ else:
 print("Counting from 1 to 3:")
 for i in range(1, 4):  # range(start, end) generates numbers up to (end-1)
     print(i)
+// Second code beigns here //
+    # Program to check if a number is prime or not
+
+num = 29
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+# define a flag variable
+flag = False
+
+if num == 0 or num == 1:
+    print(num, "is not a prime number")
+elif num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+
+    # check if flag is True
+    if flag:
+        print(num, "is not a prime number")
+    else:
+        print(num, "is a prime number")
